9fae8e6274e81ac802b811c8f878e905ff95e094
mk-pmb <mk-pmb@users.noreply.github.com>
2026-09-05 05:29:49 +0200

    Scene loading seems to work, navigation doesn't.

--- src/init.js
+++ src/init.js
@@ -18,8 +18,45 @@ function oneLineJson(x) {
 const how = {};
 
 (async function fallible() {
+  const LIB = window.MODULES;
+  const ThreeJS = LIB.get('three', 'stage2');
+  const { ColladaLoader } = LIB.exJsm('loaders/ColladaLoader.js');
+  const { PointerLockControls } = LIB.exJsm('controls/PointerLockControls.js');
+
   how.modelFileUrl = query.model;
   if (!how.modelFileUrl) { throw new Error('No model file selected.'); }
+
+  const scene = new ThreeJS.Scene();
+  scene.background = new ThreeJS.Color(0x20252b);
+
+  const { innerWidth: sceneW, innerHeight: sceneH } = window;
+  const camera = new ThreeJS.PerspectiveCamera(
+    70,
+    sceneW / sceneH,
+    0.01,
+    1e5,
+  );
+
+  camera.position.set(0, 1.7, 0);
+
+  const renderer = new ThreeJS.WebGLRenderer({ antialias: true });
+  renderer.setSize(sceneW, sceneH);
+  document.body.innerHTML = ''; // Discard the "loading" message.
+  document.body.appendChild(renderer.domElement);
+
+  scene.add(new ThreeJS.HemisphereLight(0xFFFFFF, 0x444444, 2));
+
+  const controls = new PointerLockControls(camera, renderer.domElement);
+  scene.add(controls.object);
+  controls.lock();
+
+  (new ColladaLoader()).load(how.modelFileUrl,
+    result => scene.add(result.scene));
+
+  (function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }());
 
 }()).then(null, function onFail(err) {
   document.body.innerHTML = '<h2>Error:</h2><p></p>';
